f471e48a40fd95582d2bc534f879cd375b71bdba
tradecoder <thetradecoder@gmail.com>
2022-05-01 16:33:57 +0600

    updated signup screen

--- App.tsx
+++ App.tsx
@@ -11,9 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: 'orange' }, headerTitleStyle: { color: 'black' }, cardStyle: { backgroundColor: 'white' } }}>
-        <>
-          <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Login" }} />
-          <Stack.Screen name="Signup" component={SignupScreen} options={{ title: "Sign up" }} />
+        <>        
+        <Stack.Screen name="Signup" component={SignupScreen} options={{ title: "Sign up" }} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Login" }} />
         </>
       </Stack.Navigator>
     </NavigationContainer>
--- app/screens/signupScreen.tsx
+++ app/screens/signupScreen.tsx
@@ -1,11 +1,81 @@
-import React from "react";
-import { NativeBaseProvider, Text } from "native-base";
+import React, { useState } from "react";
+import { NativeBaseProvider, VStack, HStack, Heading, Box, Text, Button, Input, Link } from "native-base";
 
 
-export default function SignupScreen(){
+export default function SignupScreen() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [mobileNumber, setMobileNumber] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const [errorList, setErrorList]= useState([]);
+    const isValidFirstName:boolean =/../.test(firstName);  // minimum 2 chars
+    const isValidLastName:boolean = /../.test(lastName);  // minimum 2 chars
+    const isValidEmail:boolean = /.....@gmail.com/.test(email); // only gmail accepted for android
+    const isValidMobileNumber:boolean = /01[3-9]......../.test(mobileNumber); 
+    // valid only for mobile operators in Bangladesh
+    // change it with your local number pattern
+    const isValidPassword = ():boolean => {
+        // minimum 8 digit password with uppercase, lowercase and numbers pattern
+        if ((/......../).test(password)
+          && (/[A-Z]/).test(password)
+          && (/[a-z]/).test(password)
+          && (/[0-9]/).test(password)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    
+    const isDataValid = ():boolean=>{
+        if(isValidFirstName && isValidLastName && isValidEmail && isValidMobileNumber && isValidPassword()){
+            return true
+        }else {
+            return false
+        }
+    }
+
+
+    function onChangeFirstName(e: string) {
+        setFirstName(e.replace(/[^A-Za-z]/g,''))
+    }
+
+    function onChangeLastName(e: string) {
+        setLastName(e.replace(/[^A-Za-z]/g,''))
+    }
+
+    function onChangeEmail(e: string) {
+        setEmail(e)
+    }
+    function onChangeMobileNumber(e: string) {
+        setMobileNumber(e.replace(/[^0-9]/g,''))
+    }
+
+    function onChangePassword(e: string) {
+        setPassword(e)
+    }
+
+    function onPressSignup(e: any) {
+        return (alert("complete the data submit action"))
+    }
     return (
         <NativeBaseProvider>
-        <Text>Sign up</Text>
-    </NativeBaseProvider>
+            <VStack p={5} space={3}>
+                <Box>
+                    <Heading size="md" color={'blue.500'}>Sign up with real information</Heading>
+                    * Later can not be changed *
+                </Box>
+
+                <Box>
+                    <Input placeholder={"First Name"} value={firstName} size="lg" onChangeText={onChangeFirstName} />
+                    <Input placeholder={"Last Name"} value={lastName} size="lg" onChangeText={onChangeLastName} />
+                    <Input placeholder={"Email Address"} value={email} size="lg" onChangeText={onChangeEmail} />
+                    <Input placeholder={"Mobile Number"} value={mobileNumber} size="lg" onChangeText={onChangeMobileNumber} />
+                    <Input placeholder={"Password"} value={password} size="lg" onChangeText={onChangePassword} />
+                    <Button onPress={onPressSignup} _text={{ fontSize: 20 }} size='lg' colorScheme={'yellow'}>Signup</Button>
+                </Box>
+            </VStack>
+        </NativeBaseProvider>
     )
 }
